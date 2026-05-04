6c2230d5451b8f7078858ce3d93668c087d9cd40
emmal8294 <156854431+emmal8294@users.noreply.github.com>
2026-05-04 14:52:44 -0700

    Update curate_PRJEB63179.py

--- curation/scripts/per_study/curate_PRJEB63179.py
+++ curation/scripts/per_study/curate_PRJEB63179.py
@@ -15,16 +15,26 @@ df_sra = pd.read_csv(sra_cleaned_path, sep="\t")
 df_curated = pd.read_csv(curated_cleaned_path, sep=",")
 df_original = pd.read_csv(original_metadata_path, sep=",")
 
-df_curated_PRJEB63179 =  df_curated[df_curated['Study_ID'] == "PRJEB63179"]
-df_original_subject = df_original[df_original["study_bioproject"] == "PRJEB63179"][["sample_accession", "sample_sample-name"]]
+df_curated_PRJEB63179 = df_curated[df_curated['Study_ID'] == "PRJEB63179"]
+df_original_subject = df_original[df_original["study_bioproject"] == "PRJEB63179"][["sample_accession", "sample_sample-name","sample_sample-desc","sample_biosample"]]
 df_sra_subject = df_sra[df_sra['bioproject'] == "PRJNA673918"]
 sample_accession_subject_map = df_original_subject.set_index("sample_accession").to_dict()["sample_sample-name"]
 sample_accession_nucleotide_map = df_sra_subject[["sample_accession", "nucleotide_type"]].set_index("sample_accession").to_dict()["nucleotide_type"]
+sample_accession_age_map = ( df_sra_subject[["sample_accession", "age_years"]].set_index("sample_accession").to_dict()["age_years"])
+sample_accession_sex_map = (df_sra_subject[["sample_accession", "sex"]].set_index("sample_accession").to_dict()["sex"])
+sample_accession_diet_map = (df_sra_subject[["sample_accession", "diet"]].set_index("sample_accession").to_dict()["diet"])
+sample_accession_health_map = (df_sra_subject[["sample_accession", "health_condition"]].set_index("sample_accession").to_dict()["health_condition"])
+sample_accession_location_map = (df_sra_subject[["sample_accession", "geo_location"]].set_index("sample_accession").to_dict()["geo_location"])
+sample_accession_lifestyle_map = (df_sra_subject[["sample_accession", "lifestyle"]].set_index("sample_accession").to_dict()["lifestyle"])
+
 
 df_curated_PRJEB63179["Subject_ID"] = df_curated_PRJEB63179["Sample_accession"].map(sample_accession_subject_map)
 df_curated_PRJEB63179["Nucleotide_Type"] = df_curated_PRJEB63179["Sample_accession"].map(sample_accession_nucleotide_map)
 df_curated_PRJEB63179["Body_site"] = "gut"
 df_curated_PRJEB63179["Body_site_core"] = "gut"
-df_curated_PRJEB63179["Nucleotide_Type"] = "DNA"
-
+#df_curated_PRJEB53689["Nucleotide_Type"] = "DNA"
+df_curated_PRJEB63179["Age"] = df_curated_PRJEB63179["Sample_accession"].map(sample_accession_age_map)
+df_curated_PRJEB63179["Sex"] = df_curated_PRJEB63179["Sample_accession"].map(sample_accession_sex_map)
+df_curated_PRJEB63179["Health_status"] = df_curated_PRJEB63179["Sample_accession"].map(sample_accession_health_map)
+df_curated_PRJEB63179["Location"] = df_curated_PRJEB63179['Sample_accession'].map(sample_accession_location_map)
 df_curated_PRJEB63179.to_csv("/content/drive/Shareddrives/Meta2/Metadata/Final_Curated_Out/PRJEB63179.csv", index = None)
