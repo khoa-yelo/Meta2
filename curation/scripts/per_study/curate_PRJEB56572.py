afda96dafeb6a1867610d95ee3c64023c0740fb8
emmal8294 <156854431+emmal8294@users.noreply.github.com>
2026-05-05 19:11:38 -0700

    Update curate_PRJEB56572.py

--- curation/scripts/per_study/curate_PRJEB56572.py
+++ curation/scripts/per_study/curate_PRJEB56572.py
@@ -15,21 +15,27 @@ df_sra = pd.read_csv(sra_cleaned_path, sep="\t")
 df_curated = pd.read_csv(curated_cleaned_path, sep=",")
 df_original = pd.read_csv(original_metadata_path, sep=",")
 
-df_curated_PRJEB56572=  df_curated[df_curated['Study_ID'] == "PRJEB56572"]
-df_original_subject = df_original[df_original["study_bioproject"] == "PRJEB56572"][["sample_accession", "sample_sample-name"]]
+df_curated_PRJEB56572 = df_curated[df_curated['Study_ID'] == "PRJEB56572"]
+df_original_subject = df_original[df_original["study_bioproject"] == "PRJEB56572"][["sample_accession", "sample_sample-name","sample_sample-desc","sample_biosample"]]
 df_sra_subject = df_sra[df_sra['bioproject'] == "PRJNA791464"]
 sample_accession_subject_map = df_original_subject.set_index("sample_accession").to_dict()["sample_sample-name"]
 sample_accession_nucleotide_map = df_sra_subject[["sample_accession", "nucleotide_type"]].set_index("sample_accession").to_dict()["nucleotide_type"]
 sample_accession_age_map = ( df_sra_subject[["sample_accession", "age_years"]].set_index("sample_accession").to_dict()["age_years"])
 sample_accession_sex_map = (df_sra_subject[["sample_accession", "sex"]].set_index("sample_accession").to_dict()["sex"])
+sample_accession_diet_map = (df_sra_subject[["sample_accession", "diet"]].set_index("sample_accession").to_dict()["diet"])
+sample_accession_health_map = (df_sra_subject[["sample_accession", "health_condition_potential"]].set_index("sample_accession").to_dict()["health_condition_potential"])
+sample_accession_location_map = (df_sra_subject[["sample_accession", "geo_location"]].set_index("sample_accession").to_dict()["geo_location"])
+sample_accession_lifestyle_map = (df_sra_subject[["sample_accession", "lifestyle"]].set_index("sample_accession").to_dict()["lifestyle"])
 
 df_curated_PRJEB56572["Subject_ID"] = df_curated_PRJEB56572["Sample_accession"].map(sample_accession_subject_map)
 df_curated_PRJEB56572["Nucleotide_Type"] = df_curated_PRJEB56572["Sample_accession"].map(sample_accession_nucleotide_map)
-df_curated_PRJEB56572["Body_site"] = "saliva"
-df_curated_PRJEB56572["Body_site_core"] = "saliva"
-df_curated_PRJEB56572["Nucleotide_Type"] = "DNA"
-df_curated_PRJEB56572["Location"] = "China"
-df_curated_PRJEB56572["Age_group"] = "Child"
-df_curated_PRJEB56572["Lifestyle"] = "no less than 18 teeth, no orthodontic appliances and accessories, no use of antibiotics, probiotics, synbiotics or fluoridated toothpaste and microecological regulators within the recent three months"
+df_curated_PRJEB56572["Body_site"] = "oral"
+df_curated_PRJEB56572["Body_site_core"] = "oral"
+#df_curated_PRJEB53689["Nucleotide_Type"] = "DNA"
+df_curated_PRJEB56572["Age"] = df_curated_PRJEB56572["Sample_accession"].map(sample_accession_age_map)
+df_curated_PRJEB56572["Sex"] = df_curated_PRJEB56572["Sample_accession"].map(sample_accession_sex_map)
+df_curated_PRJEB56572["Health_status"] = df_curated_PRJEB56572["Sample_accession"].map(sample_accession_health_map)
+df_curated_PRJEB56572["Location"] = df_curated_PRJEB56572["Sample_accession"].map(sample_accession_location_map)
+df_curated_PRJEB56572["Age_group"] = "Child"cological regulators within the recent three months"
 
 df_curated_PRJEB56572.to_csv("/content/drive/Shareddrives/Meta2/Metadata/Final_Curated_Out/PRJEB56572.csv", index = None)
